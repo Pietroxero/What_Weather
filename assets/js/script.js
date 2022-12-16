110793b1d49974c46ff2b30dd403d8477042e6d6
Pietroxero <115331791+Pietroxero@users.noreply.github.com>
2022-12-15 17:09:53 -0800

    built out funtion for processing the lon and lat data and built a for loop to generate the 5 day forecast

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,11 +3,11 @@
 var requestUrl = "api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}";
 var APIKey = "ba85e2551d84032dd058206d20a27efe";
 var city = $('#city');
-var current = $('#current');
+var currently = $('#current');
 var temp =$('#temp');
 var humidity = $('humidity');
-var wind = $('#wind');
-var fiveDay =$('#fiveDay');
+var windy = $('#wind');
+var weekDays =$('#fiveDay');
 var searching = $('#searchBtn');
 var useIn = $('#userInput');
 var longitude = "";
@@ -47,6 +47,25 @@ function getWeatherResults (lon,lat){
     .then(function (data){
         console.log(data)
     })
+ temp.text(`Current Temp: ${data.currently.temp}`);
+ windy.text(`Wind: ${data.currently.wind.speed} MPH`);
+ humidity.text(`Humidity: ${data.currently.wind.humidity}%`);
 
+ //repeat in a loop to create cards for the preceding 5 day forecast
+weekDays.empty();
 
+console.log(data.daily)
+var dazeArray = data.daily
+for (var i = 0; i<5; i++){
+    var iconData=dazeArray[i].weather[0].icon;
+    var iurl = "https://openweathermap.org/img/wn/" + iconData + ".png";
+    fetch(iurl)
+    .then(data => {
+$("fiveDayIcon").attr('src', data.url);
+    });
+    fivescards(getDate(dailyArray[i].dt), iurl, dailyArray[i].temp.max, dailyArray[i].temp.min,
+    dailyArray[i].wind.speed, dailyArray[i].humidity);
 }
+
+};
+
