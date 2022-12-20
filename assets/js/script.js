0b8e041a1bd35512e4ee52124e11f4e879049631
Pietroxero <115331791+Pietroxero@users.noreply.github.com>
2022-12-19 18:32:34 -0800

    greyed out a section of code within get weather results and edits made to elements that are going to be appended

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,99 +1,107 @@
 //setting variables to be used
 
-var requestUrl = "api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}";
+var requestUrl =
+  "api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}";
 var APIKey = "ba85e2551d84032dd058206d20a27efe";
-var city = $('#city');
-var currently = $('#current');
-var temp =$('#temp');
-var humidity = $('humidity');
-var windy = $('#wind');
-var weekDays =$('#fiveDay');
-var searching = $('#searchBtn');
-var useIn = $('#userInput');
+var city = $("#city");
+var currently = $("#current");
+var temp = $("#temp");
+var humidity = $("humidity");
+var windy = $("#wind");
+var weekDays = $("#fiveDay");
+var searching = $("#searchBtn");
+var useIn = $("#userInput");
 var longitude = "";
 var latitude = "";
 
 //event listeners for search btn or userinput
-searching.on('click', onSearchClick)
+searching.on("click", onSearchClick);
 function onSearchClick() {
-    var cityname = useIn.val()
-getLongitudeLatitude(cityname);
-};
-
-
+  var cityname = useIn.val();
+  getLongitudeLatitude(cityname);
+}
 
 //new function takes city name from search btn
-function getLongitudeLatitude (name) {
-    console.log(name)
-    fetch (`https://api.openweathermap.org/data/2.5/forecast?q=${name}&appid=${APIKey}`)
-    .then(function (response){
-        // console.log(response.json())
-        return response.json();
+function getLongitudeLatitude(name) {
+  console.log(name);
+  fetch(
+    `https://api.openweathermap.org/data/2.5/forecast?q=${name}&appid=${APIKey}`
+  )
+    .then(function (response) {
+      // console.log(response.json())
+      return response.json();
     })
-    .then(function (data){
-        console.log(data)
-        longitude = data.city.coord.lon;
-        latitude = data.city.coord.lat;
-        console.log(longitude, latitude)
-        getWeatherResults(longitude, latitude);
-    })
+    .then(function (data) {
+      console.log(data);
+      longitude = data.city.coord.lon;
+      latitude = data.city.coord.lat;
+      console.log(longitude, latitude);
+      getWeatherResults(longitude, latitude);
+    });
 }
 
 //build next function to process long and lat
-function getWeatherResults (lon,lat){
-    console.log(lon,lat)
-    fetch (`https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${APIKey}`)
-.then(function (response){
-    return response.json();
-})
-    .then(function (data){
-        console.log(data)
+function getWeatherResults(lon, lat) {
+  console.log(lon, lat);
+  var result = fetch(
+    `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&units=imperial&appid=${APIKey}`
+  )
+    .then(function (response) {
+      return response.json();
     })
- temp.text(`Current Temp: ${data.currently.temp}`);
- windy.text(`Wind: ${data.currently.wind.speed} MPH`);
- humidity.text(`Humidity: ${data.currently.wind.humidity}%`);
+    .then(function (data) {
+      console.log(data);
 
- //repeat in a loop to create cards for the preceding 5 day forecast
-weekDays.empty();
+      // check the response from API to find the fields on data object, should be in the "list" and choose the "12" for noon
+    //   temp.text(`Current Temp: ${data.currently.temp}`);
+    //   windy.text(`Wind: ${data.currently.wind.speed} MPH`);
+    //   humidity.text(`Humidity: ${data.currently.wind.humidity}%`);
 
-console.log(data.daily)
-var dazeArray = data.daily
-for (var i = 0; i<5; i++){
-    var iconData=dazeArray[i].weather[0].icon;
-    var iurl = "https://openweathermap.org/img/wn/" + iconData + ".png";
-    fetch(iurl)
-    .then(data => {
-$("fiveDayIcon").attr('src', data.url);
+      //repeat in a loop to create cards for the preceding 5 day forecast
+      weekDays.empty();
+
+    //   console.log(data.daily);
+    //   var dazeArray = data.daily;
+    //   for (var i = 0; i < 5; i++) {
+    //     var iconData = dazeArray[i].weather[0].icon;
+    //     var iurl = "https://openweathermap.org/img/wn/" + iconData + ".png";
+    //     fetch(iurl).then((data) => {
+    //       $("fiveDayIcon").attr("src", data.url);
+    //     });
+    //     fivescards(
+    //       getDateEl(dailyArray[i].dt),
+    //       iurl,
+    //       dailyArray[i].temp.max,
+    //       dailyArray[i].temp.min,
+    //       dailyArray[i].wind.speed,
+    //       dailyArray[i].humidity
+    //     );
+    //   }
     });
-    fivescards(getDateEl(dailyArray[i].dt), iurl, dailyArray[i].temp.max, dailyArray[i].temp.min,
-    dailyArray[i].wind.speed, dailyArray[i].humidity);
 }
-
-};
 
 //search history
 var dataCity = [];
 
-function addRecentSearch(cityname){
-    $("#recently-searched-list").show();
+function addRecentSearch(cityname) {
+  $("#recently-searched-list").show();
 
-//new element
-var cityQuery = $("<li>");
-cityQuery.addClass("list-group-item");
-cityQuery.text(cityname);
-//append the item
-$("#recently-searched-list").prepend(cityQuery);
+  //new element
+  var cityQuery = $("<li>");
+  cityQuery.addClass("list-group-item");
+  cityQuery.text(cityname);
+  //append the item
+  $("#recently-searched-list").prepend(cityQuery);
 
-//new object
-var objectCities = {
-    input:cityname
-};
+  //new object
+  var objectCities = {
+    input: cityname,
+  };
 
-citiesData.push(objectCities);
+  citiesData.push(objectCities);
 
-//localstorage
-localStorage.setItem("searches", JSON.stringify(citiesData));
-
+  //localstorage
+  localStorage.setItem("searches", JSON.stringify(citiesData));
 }
 
 //load for recent searches from localstorage
@@ -101,53 +109,54 @@ getSearches();
 
 // get recently searched items
 function getSearches() {
-var searches = JSON.parse(localStorage.getItem("searches"));
-if(searches != null) {
-    for (var i =0; i<searches.length; i++){
-        //create element to append
-        var newPlace = $("<li>");
-        newPlace.addClass("list-group-item");
-        newPlace.text(searches[i].input);
-        //list to append
-        $("#recently-searched-list").prepend(newPlace);
+  var searches = JSON.parse(localStorage.getItem("searches"));
+  if (searches != null) {
+    for (var i = 0; i < searches.length; i++) {
+      //create element to append
+      var newPlace = $("<li>");
+      newPlace.addClass("list-group-item");
+      newPlace.text(searches[i].input);
+      //list to append
+      $("#recently-searched-list").prepend(newPlace);
     }
     $("#recent").show();
-}
-else {
+  } else {
     $("recent").hide();
-}
-
+  }
 }
 
 //clear button function
-$("#clearhistoryBtn").on("click", function(){
-localStorage.clear();
-$("recently-searched-list").empty();
+$("#clearhistoryBtn").on("click", function () {
+  localStorage.clear();
+  $("recently-searched-list").empty();
 });
 
 //function that will take user input city, state, country using fetch for longitude, latitude
-function usersData(){
-    var city_user = useIn.val();
-    stormURL = 'https://api.openweathermap.org/data/2.5/weather?q=' + city_user + `&units=imperial&appid=${APIKey}`;
-    console.log(stormURL);
-    then(response => response.JSON())
-    then(data => {
-        longitude = data.city.coord.lon;
-        latitude = data.city.coord.lat;
-        var icoding=data.weather[0].icon;
-        var iurl = "https://openweathermap.org/img/wn/" + icoding + ".png";
-        fetch(iurl)
-        then (data => {
-            icon.attr('src', data.url)
-        });
-        city.text(`${data.name} (${getDateEl(data.dt)})`);
-        getWeatherResults();
+function usersData() {
+  var city_user = useIn.val();
+  stormURL =
+    "https://api.openweathermap.org/data/2.5/weather?q=" +
+    city_user +
+    `&units=imperial&appid=${APIKey}`;
+  console.log(stormURL);
+  then((response) => response.JSON());
+  then((data) => {
+    longitude = data.city.coord.lon;
+    latitude = data.city.coord.lat;
+    var icoding = data.weather[0].icon;
+    var iurl = "https://openweathermap.org/img/wn/" + icoding + ".png";
+    fetch(iurl);
+    then((data) => {
+      icon.attr("src", data.url);
     });
-};
+    city.text(`${data.name} (${getDateEl(data.dt)})`);
+    getWeatherResults();
+  });
+}
 
 //these are meant to manipulate our DOM elements.
 function fivescards(date, tempH, tempL, windSpeed, humidity) {
-    fiveDayWeek.append(`<div class="card d-inline-flex mx-3" style ="width: 13rem; border-radius: 30px; background-color: grey;">
+  fiveDayWeek.append(`<div class="card d-inline-flex mx-3" style ="width: 13rem; border-radius: 30px; background-color: grey;">
     <div class ="card-body text-center" id='fivescards'>
   <h5 class = "card-title" id = 'card-title'>${date}</h5>
   <h6 class = "card-subtitle mb-2 text-muted" id = 'temp'>High ${tempH}</h6>
@@ -156,36 +165,57 @@ function fivescards(date, tempH, tempL, windSpeed, humidity) {
   <h6 class = "card-subtitle mb-2 text-muted" id = 'temp'>High ${humidity}</h6>  
   </div>
   </div>`);
-};
+}
 
 //function will be for established time (day, month year)
-function getDateEl (unix_time){
-var currentdate = new Date(unix_time*1000);
-var mm = date.getMm()+1;
-var dd = date.getDd();
-var yyyy = date.getFullYear();
-return`${yyyy}/${dd}/${mm}`;
+function getDateEl(unix_time) {
+  var currentdate = new Date(unix_time * 1000);
+  var mm = date.getMm() + 1;
+  var dd = date.getDd();
+  var yyyy = date.getFullYear();
+  return `${yyyy}/${dd}/${mm}`;
 }
 
 //this loop is for the preset buttons for cities meant to update weather
-var placeNames = ['Seattle', 'Chicago', 'Los Angeles', 'Orlando', 'San Francisco', 'Austin', 'Portland', 'Atlanta']
-var uscity = [$('Washington'), $('Illinois'), $('Cali'), $('Florida'), $('San'), $('Texas'), $('Port'), $('Georgia')]
-for (var i=0; i < uscity.length && placeNames.length; i++){
-    uscity[i].on("click", () => {
-        stormURL= 'https://api.openweathermap.org/data/2.5/weather?q=' + placeNames[i] + `&units=imperial&appid=${APIKey}`;
-        fetch(stormURL)
-        then(response => response.JSON())
-        then(data => {
-            longitude = data.city.coord.lon;
-            latitude = data.city.coord.lat;
-            var icoding=data.weather[0].icon;
-            var iurl = "https://openweathermap.org/img/wn/" + icoding + ".png";
-            fetch(iurl)
-            then (data => {
-                icon.attr('src', data.url)
-            });
-city.text(`${data.name} (${getDateEl(data.dt)})`);
- getWeatherResults();
-        });
+var placeNames = [
+  "Seattle",
+  "Chicago",
+  "Los Angeles",
+  "Orlando",
+  "San Francisco",
+  "Austin",
+  "Portland",
+  "Atlanta",
+];
+var uscity = [
+  $("Washington"),
+  $("Illinois"),
+  $("Cali"),
+  $("Florida"),
+  $("San"),
+  $("Texas"),
+  $("Port"),
+  $("Georgia"),
+];
+for (var i = 0; i < uscity.length && placeNames.length; i++) {
+  uscity[i].on("click", () => {
+    stormURL =
+      "https://api.openweathermap.org/data/2.5/weather?q=" +
+      placeNames[i] +
+      `&units=imperial&appid=${APIKey}`;
+    fetch(stormURL);
+    then((reponse) => response.JSON());
+    then((data) => {
+      longitude = data.city.coord.lon;
+      latitude = data.city.coord.lat;
+      var icoding = data.weather[0].icon;
+      var iurl = "https://openweathermap.org/img/wn/" + icoding + ".png";
+      fetch(iurl);
+      then((data) => {
+        icon.attr("src", data.url);
+      });
+      city.text(`${data.name} (${getDateEl(data.dt)})`);
+      getWeatherResults();
     });
-};
+  });
+}
